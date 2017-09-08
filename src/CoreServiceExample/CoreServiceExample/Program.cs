c6cf9df0216a2a7cd1fd3c827911193635211360
Mark Davies <mark.davies@esendex.com>
2017-09-08 17:24:35 +0100

    Adding initial files

---
+++ src/CoreServiceExample/CoreServiceExample/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Hosting.WindowsServices;
+
+
+namespace CoreServiceExample
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var pathToContentRoot = Directory.GetCurrentDirectory();
+            var isService = !(Debugger.IsAttached || args.Contains("--console"));
+            if (isService)
+            {
+                var pathToExe = Process.GetCurrentProcess().MainModule.FileName;
+                pathToContentRoot = Path.GetDirectoryName(pathToExe);
+            }
+
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseContentRoot(pathToContentRoot)
+                .UseIISIntegration()
+                .UseStartup<Startup>()
+                .Build();
+
+            if (isService)
+            {
+                host.RunAsService();
+            }
+            else
+            {
+                host.Run();
+            }
+        }
+    }
+}
---
+++ src/CoreServiceExample/CoreServiceExample/Startup.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading.Tasks;
+using EasyNetQ;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using IServiceProvider = System.IServiceProvider;
+
+namespace CoreServiceExample
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSingleton(RabbitHutch.CreateBus("host=localhost"));
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, IServiceProvider provider)
+        {
+            var bus = provider.GetService<IBus>();
+            bus.Subscribe<string>("Message", Console.WriteLine);
+
+            app.Run(_ => Task.CompletedTask);
+        }
+    }
+}
